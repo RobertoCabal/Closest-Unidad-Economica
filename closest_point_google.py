62fd3e21e2add0d9caf9a15fce4fce49a3818941
RobertoCabal <robertocabal5@gmail.com>
2020-11-28 13:49:07 -0600

    update code

--- closest_point_google.py
+++ closest_point_google.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import requests # Para acceder a la APIs
+import requests # Para acceder a la API de Google
 import re # Para usar regular expressions
 import geopandas as gpd # Para leer y manipular shapefiles
 from shapely.geometry import Point # Para crear un punto con latitud y longitud
@@ -9,11 +9,12 @@ import time
 from tabulate import tabulate
 
 
-def RadiousUnidadesEconomicas(*,path_shp_denue:str,codigo_act_dict:dict,lat:[float,list],lon:[float,list],metros=2000):
+def RadiousUnidadesEconomicas(*,path_shp_denue:str,codigo_act_dict:dict,lat:[float,list],lon:[float,list],metros=2000,google_api_key=None,google=False)->dict:
     '''
     A partir de un radio fijo cuenta el número de unidades económicas del código especificado
-    y la mínima distancia lineal al punto (posiblemente fuera de la circunferencia)
-    Si no hay unidades devuelve NaN en la distancia. 
+    y la mínima duración en coche a alguna de las unidades, no solo en la circunferencia
+    La duración se da en minutos.
+    Si no hay unidades devuelve NaN en la duración. 
     ----------
     Inputs: 
             - path_shp_denue_estado: str, path al shapefile de la denue, de preferencia de un estado específico
@@ -21,6 +22,8 @@ def RadiousUnidadesEconomicas(*,path_shp_denue:str,codigo_act_dict:dict,lat:[flo
             - lat: list o float, latitudes 
             - lon: list o float, longitudes 
             - metros: float, metros a buscar
+            - google_api_key: str, key de google para usar Distance Matrix API
+            - google: boole, si usa o no la API de Google 
     Outputs: 
             - DataFrame con duración mínima y número de unidades 
     '''
@@ -32,21 +35,13 @@ def RadiousUnidadesEconomicas(*,path_shp_denue:str,codigo_act_dict:dict,lat:[flo
     codigo_act = list(codigo_act_dict.keys())
     
     # Ver si lat,lon son iterables, sino crea una lista
-     if isinstance(lat,str): 
-        lat = float(lat)
-    
-    if isinstance(lon,str): 
-        lon = float(lon)
-
     try: 
-        for i,x in enumerate(lat): 
-            lat[i] = float(x)
+        iter(lat)
     except: 
         lat = [lat]
 
     try: 
-       for i,x in enumerate(lon): 
-            lon[i] = float(x)
+        iter(lon)
     except: 
         lon = [lon]
 
@@ -85,117 +80,65 @@ def RadiousUnidadesEconomicas(*,path_shp_denue:str,codigo_act_dict:dict,lat:[flo
                 inter = gpd.overlay(denue_codigo, circle, how='intersection')
                 # El número de unidades al rededor del punto es len(inter)
                 numero_unidades_radius.append(len(inter))
-
-                # Calculamos la distancia euclediana 
+                
+                # Usamos la API Distance Matrix de Google para medir el tiempo en vehículo a la UE más cercana
+                # (no necesariemnte en la circunferencia)
+                # La API tiene un límite de 100=(origenes)*(destinos) elementos, así que filtramos los 100 
+                # destinos más cercanos en el sentido lineal
+                # La API se usa sólo si google = True
+                # (Sé que es tonto poner el if adentro del for, pero fue mi solución rápida) 
                 denue_codigo['distance_to_point'] = denue_codigo['geometry'].apply(lambda p: np.sqrt((p.x-pt.x)**2+(p.y-pt.y)**2)/alpha)
-                # Ordenamos por la distancia de menor a mayor
                 denue_codigo = denue_codigo.sort_values(by='distance_to_point',ascending=True)
                 denue_codigo.reset_index(drop=True,inplace=True)
-                # Nos quedamos con la distancia lineal más pequeña
-                distance.append(np.round(denue_codigo.loc[0,'distance_to_point'],2))
+                denue_codigo = denue_codigo.loc[0:99,:]
+
+                if google: 
+                    # Hacemos un string con los destinos a los que se quiere medir la distancia
+                    destinations = ''
+                    for x in denue_codigo[['latitud','longitud']].values:
+                        destinations += str(x[0])+','+str(x[1])+'|'
+                    destinations = destinations[:-1]
+                    # Especificamos los otros parámetros de la API
+                    outputFormat = 'json'
+                    units = 'imperial'
+                    origins = f'{Lat},{Lon}'
+                    mode = 'driving'
+                    parameters = f'units={units}&origins={origins}&destinations={destinations}&mode={mode}&key={google_api_key}'
+                    url_google = f'https://maps.googleapis.com/maps/api/distancematrix/{outputFormat}?{parameters}'
+                    # Llamamos a la API con requests
+                    r_google = requests.get(url_google)
+                    # Leemos el resultado de la API como JSON
+                    d = r_google.json()
+                    # Almacenamos las duraciones en coche (en minutos) a cada destino, i.e., unidad económica
+                    duration = []
+                    for z in d['rows'][0]['elements']: 
+                        d = z['duration']['text']
+                        # quitamos letras y nos quedamos con números
+                        if 'hour' in d:
+                            h = re.sub('[a-z]','',d).strip().split()[0]
+                            m = re.sub('[a-z]','',d).strip().split()[1]
+                            h = float(h)
+                            m = float(m)
+                            d = 60*h+m
+                        else:
+                            d = re.sub('[a-z]','',d) 
+                            d = float(d)
+                        duration.append(d)
+                    # Obtenemos la dración mínima 
+                    duration = np.array(duration)
+                    distance.append(duration.min())
+
+                else: 
+                    distance.append(denue_codigo.loc[0,'distance_to_point'])
         
             # Devolvemos el número de unidades y la duración mínima
             df.loc[:,actividad+'_numero'] = numero_unidades_radius
-            df.loc[:,actividad+'_distancia'] = distance
+            if google: 
+                df.loc[:,actividad+'_duracion'] = distance
+            else: 
+                df.loc[:,actividad+'_distancia'] = distance
 
     return df
-
-
-def RadiousKeyWord(*,path_shp_denue:str,key_words_list:list,lat:[float,list],lon:[float,list],metros=2000):
-    '''
-    A partir de un radio fijo cuenta el número de unidades económicas con la palabra clave especificada
-    y la mínima distancia lineal al punto (posiblemente fuera de la circunferencia)
-    Si no hay unidades devuelve NaN en la distancia. 
-    ----------
-    Inputs: 
-            - path_shp_denue_estado: str, path al shapefile de la denue, de preferencia de un estado específico
-            - key_words_list: list, palabras clave sobre el nombre del establecimiento
-            - lat: list o float, latitudes 
-            - lon: list o float, longitudes 
-            - metros: float, metros a buscar
-    Outputs: 
-            - DataFrame con duración mínima y número de unidades 
-    '''
-
-    # Cargamos el shapefile del DENUE 
-    denue = gpd.read_file(path_shp_denue)
-
-    # Convertimos en lista las palabras clave 
-    if isinstance(key_words_list, list):
-        pass 
-    else: 
-        key_words_list = [key_words_list] 
-    
-    # Ver si lat,lon son iterables, sino crea una lista
-    if isinstance(lat,str): 
-        lat = float(lat)
-    
-    if isinstance(lon,str): 
-        lon = float(lon)
-
-    try: 
-        for i,x in enumerate(lat): 
-            lat[i] = float(x)
-    except: 
-        lat = [lat]
-
-    try: 
-       for i,x in enumerate(lon): 
-            lon[i] = float(x)
-    except: 
-        lon = [lon]
-
-    # DataFrame de resultados
-    df = pd.DataFrame()
-
-    for k in key_words_list: 
-        # Filtramos el DENUE con el nombre clave 
-        k = k.strip().upper()
-        denue_codigo = denue[denue['nom_estab'].str.contains(k)].copy()
-
-        # Si no hay esta unidad en el estado devolvemos 0's y NaN's
-        if len(denue_codigo)==0:
-            numero_unidades_radius = [0 for _ in range(len(lat))]
-            distance = [np.nan for _ in range(len(lat))]
-        else: 
-            # Cambiamos el sistema de coordenadas
-            denue_codigo.to_crs("EPSG:4326",inplace=True)
-
-            numero_unidades_radius = []
-            distance = []
-            for Lat,Lon in zip(lat,lon):
-                # Creamos un punto el la coordenada proporcionada
-                pt = gpd.GeoSeries([Point(Lon,Lat)])
-
-                # alpha es una constante para traducir metros en el crs especificado
-                alpha = 0.005/550
-                # Creamos la circunferencia con los metros especificados
-                circle = pt.buffer(metros*alpha)
-                # Convertimos el punto en un GeoDataFrame 
-                circle = gpd.GeoDataFrame(circle,columns=['geometry'])
-                # Cambiamos el sistema de coordenadas para que coicidan
-                circle.set_crs("EPSG:4326",inplace=True)
-                # Obtenemos la base con las intersecciones geográficas (puntos que caen en la circunferencia)
-                inter = gpd.overlay(denue_codigo, circle, how='intersection')
-                # El número de unidades al rededor del punto es len(inter)
-                numero_unidades_radius.append(len(inter))
-
-                # Calculamos la distancia euclediana 
-                denue_codigo['distance_to_point'] = denue_codigo['geometry'].apply(lambda p: np.sqrt((p.x-pt.x)**2+(p.y-pt.y)**2)/alpha)
-                # Ordenamos por la distancia de menor a mayor
-                denue_codigo = denue_codigo.sort_values(by='distance_to_point',ascending=True)
-                denue_codigo.reset_index(drop=True,inplace=True)
-                # Nos quedamos con la distancia lineal más pequeña
-                distance.append(np.round(denue_codigo.loc[0,'distance_to_point'],2))
-        
-            # Devolvemos el número de unidades y la duración mínima
-            df.loc[:,actividad+'_numero'] = numero_unidades_radius
-            df.loc[:,actividad+'_distancia'] = distance
-
-    return df
-
-
-
 
 if __name__=='__main__':
     # Estado: Yucatán 
@@ -212,6 +155,11 @@ if __name__=='__main__':
     lat = [21.015963,20.994841]
     lon = [-89.590495,-89.612894]
     metros = 1000
+
+    # Para leer mi API key del archivo yaml
+    with open("google_api_keys.yaml","r") as f:
+        keys = yaml.load(f,Loader=yaml.FullLoader)
+        google_api_key = keys['Distance Matrix']
 
     start = time.time()
     rue = RadiousUnidadesEconomicas(path_shp_denue=path_shp_denue,
--- places_data.py
+++ places_data.py
@@ -9,17 +9,25 @@ import pandas as pd
 from closest_point import RadiousUnidadesEconomicas
 import time
 
+# Cargamos el shapefile
+estado = 9
 with open('estado_shapefile.yaml') as f: 
     path = yaml.load(f,Loader=yaml.FullLoader)
-    path = path['shp_mza_31']
+    path = path[f'shp_mza_{estado}']
 shp_mza = gpd.read_file(path)
+# Cambiamos el crs
 shp_mza.to_crs('EPSG:4326',inplace=True)
-shp_mza = shp_mza[shp_mza['CVE_MUN']=='050']
+# Filtramos por municipio
+# mun = '050'
+# shp_mza = shp_mza[shp_mza['CVE_MUN']==mun]
 
 start = time.time()
-n_samples = 100
+# Tomamos n_sample manzanas al azar
+n_samples = 200
 shp_mza = shp_mza.sample(n_samples,random_state=12345).reset_index(drop=True)
+# Calculamos el centroide
 shp_mza['centroid'] = shp_mza.centroid
+# Guardamos las coordenadas
 latitud = []
 longitud = []
 for p in shp_mza['centroid']:
@@ -27,6 +35,10 @@ for p in shp_mza['centroid']:
     longitud.append(p.x)
 points = pd.DataFrame({'CVEGEO':shp_mza['CVEGEO'],'latitud':latitud,'longitud':longitud})
 
+end = time.time()
+print('Simulation running time: {:.2f} minutes'.format((end-start)/60))
+
+# Definimos las unidades economicas con un diccionario
 # unidades_economicas = {'supermercados':'462111','minisupers':'462112','vinos_licores':'461211',
 #                        'cerveza':'461212','corporativos':'551111','primarias_privadas':'611121',
 #                        'primarias_publicas':'611122','hospitales_privados':'622111','hospitales_publicos':'622112'}
@@ -34,30 +46,35 @@ points = pd.DataFrame({'CVEGEO':shp_mza['CVEGEO'],'latitud':latitud,'longitud':l
 
 unidades_economicas = {'522110':'bancos','primarias_privadas':'611121','611131':'secundarias_privadas',
                        '462111':'supermercados','463310':'calzado_minoreo','463211':'ropa_minoreo',
-                       '464121':'lentes_minoreo','622111':'hospitales_privados','461212':'cerveza'}
+                       '464121':'lentes_minoreo','622111':'hospitales_privados','512130':'cines',
+                       '722511':'restaurantes','722515':'cafeterias','721111':'hoteles'}
 
+# Creamos el data frame a imprimir 
 df = pd.DataFrame()
 df['CVEGEO'] = points['CVEGEO']
 df['latitud'] = points['latitud']
 df['longitud'] = points['longitud']
 
+# Leemos el shapefile del DENUE
 with open("denue_shapefile.yaml") as f: 
         path = yaml.load(f,Loader=yaml.FullLoader)
-path_shp_denue = path['denue_31']
+path_shp_denue = path[f'denue_{estado}']
 
 lat = df['latitud']
 lon = df['longitud']
 metros = 1000
-codigo_act = list(unidades_economicas.values())
+
+# Creamos los fetures de número de unidades y distancia 
+start = time.time()
 rue = RadiousUnidadesEconomicas(path_shp_denue=path_shp_denue,
                                 codigo_act_dict=unidades_economicas,
                                 lat=lat,lon=lon,
                                 metros=metros)
+end = time.time()
+print('Features running time: {:.2f} minutes'.format((end-start)/60))
 
 df = pd.concat([df,rue],axis=1)
 
-end = time.time()
-print('Running time: {:.2f} seconds'.format(end-start))
-
+# Exportamos el csv
 df.to_csv('casas_sample.csv',index=False)
 
